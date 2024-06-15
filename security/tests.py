ca75a846ba3f2394acd8020ebdd9ba1d915ec762
demetre <deme.barbakadze2@gmail.com>
2024-06-15 11:37:25 +0400

    test bug resolved

--- security/tests.py
+++ security/tests.py
@@ -16,13 +16,13 @@ class ChannelTests(APITestCase):
         self.sender_token = RefreshToken.for_user(self.sender).access_token
         self.recipient_token = RefreshToken.for_user(self.recipient).access_token
 
-    def create_channel_test(self):
+    def test_create_channel(self):
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.sender_token}')
         response = self.client.post('api/channels/', {'recipient_user': self.recipient.id})
         self.assertEqual(response.status_code, 201)
         self.assertIn('id', response.data)
 
-    def accept_channel_test(self):
+    def test_accept_channel(self):
         channel = Channel.objects.create(sender_user=self.sender, recipient_user=self.recipient, name='test')
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.recipient_token}')
         response = self.client.post('/api/channels/accept_channel', {
@@ -31,7 +31,7 @@ class ChannelTests(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('recipient_secret', response.data)
 
-    def generate_key_test(self):
+    def test_generate_key(self):
         channel = Channel.objects.create(sender_user=self.sender, recipient_user=self.recipient, name='test', accepted=True, initial_sender_secret=1234)
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.sender_token}')
         response = self.client.post('/api/generate-key/', {'channel_id': channel.id, 'secret_key': '1234567890abcdef'})
